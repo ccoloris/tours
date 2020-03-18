7f5c3893b5ec2cec156eaf23aff44622f5fe1b67
Danil <dabdullinp@gmail.com>
2020-03-18 15:04:49 +0500

    init commit

---
+++ tours/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views import View
+
+
+class MainView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
+
+
+class DepartureView(View):
+    def get(self, request, departure,*args, **kwargs):
+        return render(request, 'departure.html')
+
+
+class TourView(View):
+    def get(self, request, id, *args, **kwargs):
+        return render(request, 'tour.html')
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/dacoloris/Документы/УМ/stepik/stepik_tours/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
